90e113f8fb3e95b74fd7f99596f05469a6de258c
Luca Sorace Stranck <strdjn@gmail.com>
2016-07-14 21:30:06 +0200

    Add files via upload

---
+++ TubiInfami/src/tubi/bastarti/Main.java
@@ -0,0 +1,150 @@
+package tubi.bastarti;
+
+import java.util.Arrays;
+import java.util.Date;
+import java.util.Random;
+import java.util.Scanner;
+
+import org.fusesource.jansi.Ansi.Color;
+import org.fusesource.jansi.AnsiConsole;
+import static org.fusesource.jansi.Ansi.*;
+import static org.fusesource.jansi.Ansi.Color.*;
+
+public class Main {
+	public static boolean[] time;
+	public static int[] selection;
+	public static int[] solution;
+	public static int[] a;
+	public static int[] b;
+	public static int[] c;
+	public static int[] d;
+	public static int[] e;
+	public static int[] f;
+	public static int start;
+	public static int end;
+	public static int endext;
+	public static int[] stat;
+	public static Color[] color;
+	public static String[] out;
+	public static void main(String[] args){
+		AnsiConsole.systemInstall();
+		Startup.startup();
+		Scanner sc = new Scanner(System.in);
+		boolean sol = false;
+		long startTime = System.currentTimeMillis();
+		logger("Start at: "+startTime+"\n\nPROGRAMMED BY STRANCK http://bit.ly/SLClink\n\n");
+		/*System.out.println(Arrays.toString(a));
+		System.out.println(Arrays.toString(b));
+		System.out.println(Arrays.toString(c));
+		System.out.println(Arrays.toString(d));
+		System.out.println(Arrays.toString(e));
+		System.out.println(Arrays.toString(f));*/
+		while(true){
+			colorTrad();
+			System.out.println(ansi().fg(color[0]).a("A: "+selection[0]+out[0]).reset());
+			System.out.println(ansi().fg(color[1]).a("B: "+selection[1]+out[1]).reset());
+			System.out.println(ansi().fg(color[2]).a("C: "+selection[2]+out[2]).reset());
+			System.out.println(ansi().fg(color[3]).a("D: "+selection[3]+out[3]).reset());
+			System.out.println(ansi().fg(color[4]).a("E: "+selection[4]+out[4]).reset());
+			System.out.println(ansi().fg(color[5]).a("F: "+selection[5]+out[5]).reset());
+			//System.out.println(Arrays.toString(stat));
+			/*logger("\nA: "+selection[0]+
+					"\nB: "+selection[1]+
+					"\nC: "+selection[2]+
+					"\nD: "+selection[3]+
+					"\nE: "+selection[4]+
+					"\nF: "+selection[5]);*/
+			String in = sc.nextLine();
+			//in.equalsIgnoreCase("solution")
+			if(in.equalsIgnoreCase("solution")){
+				sol = true;
+				for(int i=0; i<6; i++){
+					selection[i] = solution[i];
+					//System.out.println(i+" "+selection[i]+" "+solution[i]);
+				}
+			}
+			String[] sp = in.split("\\s+");
+			if(toInt(sp[0])==6||Integer.parseInt(sp[1])>2) System.out.println(ansi().fg(RED).a("Invalid format.").reset()); else {
+				selection[toInt(sp[0])] = Integer.parseInt(sp[1]);
+			}
+			if(airFlow()&&sol==false){
+				long estimatedTime = System.currentTimeMillis() - startTime;
+				estimatedTime = estimatedTime/1000;
+				int hours = (int) estimatedTime / 3600;
+			    int remainder = (int) estimatedTime - hours * 3600;
+			    int mins = remainder / 60;
+			    remainder = remainder - mins * 60;
+			    int secs = remainder;
+				System.out.println(ansi().fg(YELLOW).a("You win in "+hours+":"+mins+":"+secs+"!"
+						+ "\nTHANKS FOR PLAY\nProgrammed by Stranck http://bit.ly/SLClink").reset()); 
+				return;
+				}
+			sol = false;
+		}
+	}
+	public static void colorTrad(){
+		for(int i=0; i<6; i++){
+			if(stat[i]==0) color[i] = GREEN;
+			if(stat[i]==1) color[i] = BLUE;
+			//if(stat[i]==2) {color[i] = BLUE; out[i]=" *";}
+			if(stat[i]==3) color[i] = WHITE;
+		}
+	}
+	public static boolean airFlow(){
+		int start = search(0);
+		int s;
+		int next = start;
+		boolean[] already = new boolean[6];
+		for(int i=0; i<6; i++){
+			already[i] = false;
+			if(stat[i]==1) stat[i] = 3;
+		}
+		int c = 0;
+		while(true){
+			c++;
+			//System.out.println(Arrays.toString(stat));
+			if(already[next]||c==7) return false;
+			already[next] = true;
+			s = selection[next];
+			//System.out.println(out[next]+"   "+endext);
+			if(out[next].equals(" *")&&endext==s) return true;
+			next = read(next, s);
+			if(stat[next]==3) stat[next] = 1;
+		}
+	}
+	public static int read(int v, int i){
+		if(v==0) return a[i];
+		if(v==1) return b[i];
+		if(v==2) return c[i];
+		if(v==3) return d[i];
+		if(v==4) return e[i];
+		if(v==5) return f[i];
+		return 0;
+	}
+	public static int search(int i){
+		for(int n=0; n<stat.length; n++){
+			if(stat[n]==i) return n;
+		}
+		return 0;
+	}
+	public static int toInt(String s){
+		if(s.equalsIgnoreCase("a")) return 0;
+		if(s.equalsIgnoreCase("b")) return 1;
+		if(s.equalsIgnoreCase("c")) return 2;
+		if(s.equalsIgnoreCase("d")) return 3;
+		if(s.equalsIgnoreCase("e")) return 4;
+		if(s.equalsIgnoreCase("f")) return 5;
+		return 6;
+	}
+	public static int random(int i){
+		Random generator = new Random();
+		return generator.nextInt(i);
+	}
+	public static void logger(String text) {
+		System.out.println(text);
+	}
+	public static void loggerC(String text) {
+		AnsiConsole.systemInstall();
+		System.out.print(ansi().fg(RED).a("Test"));
+	}
+}
---
+++ TubiInfami/src/tubi/bastarti/Startup.java
@@ -0,0 +1,120 @@
+package tubi.bastarti;
+
+import java.util.Arrays;
+
+import org.fusesource.jansi.Ansi.Color;
+import static org.fusesource.jansi.Ansi.Color.*;
+
+public class Startup {
+	public static void startup(){
+		Main.logger("Starting up...\nSetting up arrays.");
+		Main.selection = new int[6];
+		Main.time = new boolean[6];
+		Main.solution = new int[6];
+		Main.stat = new int[6];
+		Main.color = new Color[6];
+		Main.out = new String[6];
+		Main.a = new int[3];
+		Main.b = new int[3];
+		Main.c = new int[3];
+		Main.d = new int[3];
+		Main.e = new int[3];
+		Main.f = new int[3];
+		Main.logger("Presetting");
+		preset();
+		Main.logger("Creating a solution");
+		solution();
+		Main.logger("Choosing other pipes");
+		for(int i=0;i<6;i++){
+			for(int n=0;n<3;n++){
+				if(i==0&&Main.a[n]==8) Main.a[n] = testEq(i);
+				if(i==1&&Main.b[n]==8) Main.b[n] = testEq(i);
+				if(i==2&&Main.c[n]==8) Main.c[n] = testEq(i);
+				if(i==3&&Main.d[n]==8) Main.d[n] = testEq(i);
+				if(i==4&&Main.e[n]==8) Main.e[n] = testEq(i);
+				if(i==5&&Main.f[n]==8) Main.f[n] = testEq(i);
+			}
+		}
+	}
+	public static void solution(){
+		//boolean done = true;
+		Main.start = Main.random(6);
+		int last = Main.start;
+		int i = 0;
+		Main.stat[last] = 0;
+		Main.logger("Entering in solution loop");
+		while(true){
+			i++;
+			//System.out.println(last+" "+i);
+			if(i==6){
+				Main.end = last;
+				Main.out[last] = " *";
+				Main.endext = Main.random(3);
+				return;
+			}
+			
+			int r = solapp(last);
+			int id = Main.random(3);
+			//System.out.println(id);
+			sinapp(last, id, r);
+			Main.time[last] = true;
+			Main.solution[last] = id;
+			//System.out.println(id + " "+last);
+			//System.out.println(Arrays.toString(Main.solution));
+			last = r;
+		}
+	}
+	public static void sinapp(int s, int i, int v){
+		//System.out.println(s+" "+i+" "+v);
+		if(s==0) Main.a[i] = v;
+		if(s==1) Main.b[i] = v;
+		if(s==2) Main.c[i] = v;
+		if(s==3) Main.d[i] = v;
+		if(s==4) Main.e[i] = v;
+		if(s==5) Main.f[i] = v;
+	}
+	public static int testEq(int i){
+		int random;
+		while(true) {
+			random = Main.random(6);
+			if(random!=i) return random;
+		}
+	}
+	public static int solapp(int i){
+		int rand;
+		Main.logger("Solapping");
+		while(true){
+			rand = Main.random(6);
+			//System.out.println(rand +" "+i+" "+Main.time[rand]);
+			//System.out.println(Arrays.toString(Main.time));
+			if(rand!=i&&Main.time[rand]==false) {/*System.out.println("ret");*/ return rand;}
+			if(allOcc()) return 8;
+		}
+	}
+	public static boolean allOcc(){
+		for(int i=0; i<Main.time.length; i++){
+			if(Main.time[i]==false) return false;
+		}
+		return true;
+	}
+	public static void preset(){
+		//713231
+		for(int i=0; i<Main.selection.length; i++){
+			Main.selection[i] = 0;
+		}
+		for(int i=0; i<Main.time.length; i++){
+			Main.time[i] = false;
+			Main.color[i] = WHITE;
+			Main.stat[i] = 3;
+			Main.out[i] = "";
+		}
+		for(int i=0; i<3; i++){
+			Main.a[i] = 8;
+			Main.b[i] = 8;
+			Main.c[i] = 8;
+			Main.d[i] = 8;
+			Main.e[i] = 8;
+			Main.f[i] = 8;
+		}
+	}
+}
